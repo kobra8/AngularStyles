b245e886ce27dd202abc73ca4ba6f6bd9f64ac20
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-11-28 15:57:24 +0100

    Styles with ngClass

--- ng-styles/src/app/author/author.component.ts
+++ ng-styles/src/app/author/author.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-author',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
     h1 {
       margin: 0;
       font-size: 14px;
-      color: #8c8c8c;
+      /* color: #8c8c8c;  -> Zakomentowany kolor h1 -> stylowanie przeniesione do komponentu rodzica -> quotes-component -> host-context() "/deep/" */
       }
-  `]
+  `],
+  encapsulation: ViewEncapsulation.Emulated // Ta wartość jest domyślna i nie trzeba jej ustawiać. Angular wtedy stosuje style do każdego komponentu osobno jako Shadow DOM
 })
 export class AuthorComponent implements OnInit {
 
