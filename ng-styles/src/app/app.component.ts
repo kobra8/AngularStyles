7e60c2ea9eab208380965aa863594bd198d80fc2
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-11-29 15:54:53 +0100

    App project styles

--- ng-styles/src/app/app.component.ts
+++ ng-styles/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isfavorite:boolean = false;
+  isfavorite: boolean = false;
+
+constructor(
+  private renderer: Renderer2
+) {}
+
+  onShowBoring(element: HTMLElement) {
+   this.renderer.setStyle(element, 'display', 'block');
+  }
 }
