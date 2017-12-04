ffacd34434db65b497d3a95e1ba3da83f9fdfaec
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-12-04 16:09:27 +0100

    Angular animations module import

--- ng-animations/src/app/app.module.ts
+++ ng-animations/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { GoalsComponent } from './goals/goals.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { GoalsComponent } from './goals/goals.component';
     GoalsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
