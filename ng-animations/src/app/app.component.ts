9fb513fa05138dc8c9b977fa995e2d0144aad211
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-12-06 15:44:46 +0100

    Animation triggers and states

--- ng-animations/src/app/app.component.ts
+++ ng-animations/src/app/app.component.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'orange',
+        width: '100px',
+        height: '100px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'blue',
+        width: '300px',
+        height: '50px'
+      })),
+      transition('default => clicked', animate('2s 500ms ease-in')),
+      transition('clicked => default', animate(300))
+    ])
+  ]
 })
 export class AppComponent {
+  private triggerState = 'default';
+
+  changeState() {
+    this.triggerState = 'clicked';
+    setTimeout(() => {
+      this.triggerState = 'default';
+    }, 3000);
+  }
+
+
+
+
+
+  // Part from starting with animations
   courseGoals = [
-    {title: 'Master Angular Styling', isActiveGoal: false },
-    {title: 'Understand Angular Animations', isActiveGoal: false },
-    {title: 'Master Angular Animations', isActiveGoal: false },
+    { title: 'Master Angular Styling', isActiveGoal: false },
+    { title: 'Understand Angular Animations', isActiveGoal: false },
+    { title: 'Master Angular Animations', isActiveGoal: false },
   ];
-divClicked = false;
-doAnimate = false;
+  divClicked = false;
+  doAnimate = false;
+
 }
