37ff49fc265028f007999733c2d6df52cb74d592
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-12-08 16:07:28 +0100

    Animation temporary styles and import from external file

--- ng-animations/src/app/app.component.ts
+++ ng-animations/src/app/app.component.ts
@@ -1,39 +1,19 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations'
+import { clickedStateTrigger, numberEnteredStateTrigger } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('clickedState', [
-      state('default', style({
-        backgroundColor: 'orange',
-        width: '100px',
-        height: '100px'
-      })),
-      state('clicked', style({
-        backgroundColor: 'blue',
-        width: '300px',
-        height: '50px'
-      })),
-      state('mousedown', style({
-        backgroundColor: 'red',
-        border: '1px solid black',
-        width: '100px',
-        height: '100px'
-      })),
-      transition('default => clicked', animate('2s 500ms ease-in')),
-      transition('clicked => default', animate(300)),
-      transition('mousedown <=> clicked', animate(600)),
-      transition('default => mousedown', animate(600)),
-    //  transition('clicked => mousedown', animate(300)),
-    ])
-  ]
+    clickedStateTrigger,
+    numberEnteredStateTrigger
+   ]
 })
 export class AppComponent {
   private infoClick = 'default';
   private paragraphClick = 'default';
+  numberEntered;
 
   changeState() {
     this.infoClick = 'clicked';
